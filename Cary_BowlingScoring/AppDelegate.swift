d1d136de6054cfb2957f2c2285bcf0e3491ad0a8
Cary Stephan <cary@carys-macbook-air.local>
2017-06-22 15:22:31 -0600

    Initial Commit

---
+++ Cary_BowlingScoring/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cary_BowlingScoring
+//
+//  Created by Cary Stephan on 6/22/17.
+//  Copyright © 2017 Cary Stephan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cary_BowlingScoring/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cary_BowlingScoring
+//
+//  Created by Cary Stephan on 6/22/17.
+//  Copyright © 2017 Cary Stephan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
